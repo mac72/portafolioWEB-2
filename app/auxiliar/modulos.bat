20036bcbd37da5c2e261ededb191ff714b02f5e6
mac72 <jmsegovi72@gmail.com>
2014-10-24 14:23:39 -0500

    V1-141024-14:23
    estructura del sitio

---
+++ app/auxiliar/modulos.bat
@@ -0,0 +1,18 @@
+@echo on
+cd C:\xampp\htdocs\proyectos\portafolioWEB-2
+pause
+npm install --save-dev gulp
+pause
+npm install --save-dev gulp-connect
+npm install --save-dev connect-history-api-fallback
+npm install --save-dev gulp-jshint
+npm install --save-dev jshint-stylish
+npm install --save-dev gulp-useref
+npm install --save-dev gulp-if
+npm install --save-dev gulp-uglify
+npm install --save-dev gulp-minify-css
+npm install --save-dev gulp-stylus
+npm install --save-dev nib
+npm install --save-dev gulp-jade
+pause
+exit
